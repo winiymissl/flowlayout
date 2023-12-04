24ddcd475d81e893de449504fed5065369786602
winiymissl <winiymissl@163.com>
2023-12-04 22:37:40 +0800

    第一次修改

---
+++ app/src/main/java/com/example/beautifulspinner/SpinnerActivity.java
@@ -0,0 +1,17 @@
+package com.example.beautifulspinner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * @author winiymissl
+ */
+public class SpinnerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spinner);
+    }
+}
